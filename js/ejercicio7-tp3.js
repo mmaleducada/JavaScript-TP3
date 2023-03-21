14bf16022b06e77fdc854f2a4f9f391522c54008
Malena de Arriba <dearribamalena@gmail.com>
2023-03-21 12:47:50 -0300

    ejercicio 7 finalizado

---
+++ js/ejercicio7-tp3.js
@@ -0,0 +1,16 @@
+const filas = 11;
+let numero = parseInt(prompt("Ingrese un numero"));
+
+document.write('<table><tbody>')
+//dibuja las filas
+for (let multiplicando = 1; multiplicando < filas; multiplicando++) {
+    let resultado = numero * multiplicando;
+    document.write('<tr>')
+    document.write(`<td>${numero}</td>`)
+    document.write(`<td>x</td>`)
+    document.write(`<td>${multiplicando}</td>`)
+    document.write(`<td>=</td>`)
+    document.write(`<td>${resultado}</td>`)
+    document.write('</tr>')
+}
+document.write('</tbody></table>')
